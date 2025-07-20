51a8a31d2ab395c43f03ce608412cf868740a122
Edilson Roque <edilsonjroque@gmail.com>
2025-07-20 23:16:48 +0200

    Add cors permission

--- CarLeasing/Program.cs
+++ CarLeasing/Program.cs
@@ -11,6 +11,17 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<ICarInterface, CarService>();
 
+// Definir política CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowLocalhost4200", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200")  // Frontend autorizado
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
